697378451266d07b92bc1a44ef7e345ac238c62a
AbhishekUnthread <abhishek.k@unthread.world>
2024-01-13 10:37:16 +0530

    environment variables added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: false,
+  env: {
+    ENV: 'development',
+    BASE_URL: 'https://hn.algolia.com/api/v1',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
