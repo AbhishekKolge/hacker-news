11e0c56d332cf18cf663df105f00451091039b3c
AbhishekUnthread <abhishek.k@unthread.world>
2024-01-13 18:22:18 +0530

    news list added with filter and cards

--- src/components/home/news-list.tsx
+++ src/components/home/news-list.tsx
@@ -1,5 +1,6 @@
 'use client';
 import Filter from '@/components/ui/filter';
+import NewsCard from './news-card';
 
 import useFilters from '@/hooks/useFilter';
 
@@ -21,13 +22,16 @@ const NewsList: React.FC<NewsResult> = (props) => {
   } = useFilters();
 
   const dateRange = {
-    from: helperState.dateFrom ? new Date(helperState.dateFrom) : undefined,
-    to: helperState.dateTo ? new Date(helperState.dateTo) : undefined,
+    from: helperState.dateFrom
+      ? new Date(helperState.dateFrom * 1000)
+      : undefined,
+    to: helperState.dateTo ? new Date(helperState.dateTo * 1000) : undefined,
   };
 
   return (
     <>
       <Filter
+        firstRender={helperState.firstRender}
         search={helperState.query}
         onSearch={searchHandler}
         sortByValue={helperState.sorBy}
@@ -37,6 +41,9 @@ const NewsList: React.FC<NewsResult> = (props) => {
         dateRange={dateRange}
         onDateRange={customDateRangeHandler}
       />
+      {results.map((news) => {
+        return <NewsCard key={news.id} {...news} />;
+      })}
     </>
   );
 };
