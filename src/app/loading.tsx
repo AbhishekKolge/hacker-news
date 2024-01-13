4f1b270a38dfc3a03c399f8554890fc6a20d7e44
AbhishekUnthread <abhishek.k@unthread.world>
2024-01-13 10:43:17 +0530

    loading screen added

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+const Loading: React.FC = () => {
+  return (
+    <section className='h-full grid items-center'>
+      <p className='text-4xl text-center tracking-tight font-bold text-gray-900'>
+        Loading...
+      </p>
+    </section>
+  );
+};
+
+export default Loading;
