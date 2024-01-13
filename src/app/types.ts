9ca08d40808d59630d286254d5cbaf764aa50f4c
AbhishekUnthread <abhishek.k@unthread.world>
2024-01-14 00:43:41 +0530

    news types added

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,6 +1,14 @@
-export interface GetNewsParams {
-  queryFilter: Record<string, string>;
-  sortBy: string;
+export interface ApiResponse {
+  page: number;
+  nbPages: number;
+  hits: {
+    title: string;
+    story_id: number;
+    author: string;
+    url: string;
+    points: number;
+    num_comments: number;
+  }[];
 }
 export interface NewsItem {
   title: string;
@@ -14,20 +22,4 @@ export interface NewsResult {
   page: number;
   totalPages: number;
   results: NewsItem[];
-}
-export interface ApiResponse {
-  page: number;
-  nbPages: number;
-  hits: {
-    title: string;
-    story_id: number;
-    author: string;
-    url: string;
-    points: number;
-    num_comments: number;
-  }[];
-}
-export interface PageProps {
-  params: { slug: string };
-  searchParams: { [key: string]: string | undefined };
 }
