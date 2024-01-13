1b9bba725e37a67cacf9d169c61f1b775eaff81b
AbhishekUnthread <abhishek.k@unthread.world>
2024-01-13 19:15:10 +0530

    utility function added to get fallback initials for images

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -41,4 +41,12 @@ const pickExactObjKeys = <T extends ObjectModel, K extends keyof T>(
   return result;
 };
 
-export { omitEmptyKeys, pickExactObjKeys };
+const getInitials = (str: string): string => {
+  const words = str.split(' ');
+  const firstTwoLetters = words.map((word) => word.charAt(0)).slice(0, 2);
+  const result = firstTwoLetters.join('');
+
+  return result;
+};
+
+export { omitEmptyKeys, pickExactObjKeys, getInitials };
