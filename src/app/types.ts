16fa7b733fbdc98d24d1fdb639363af88a77345b
AbhishekUnthread <abhishek.k@unthread.world>
2024-01-14 13:57:51 +0530

    news details types added

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,4 +1,4 @@
-export interface ApiResponse {
+export interface NewsListApiResponse {
   page: number;
   nbPages: number;
   hits: {
@@ -8,8 +8,28 @@ export interface ApiResponse {
     url: string;
     points: number;
     num_comments: number;
+    created_at_i: number;
   }[];
 }
+
+export interface NewsDetailsChildrenApiResponse {
+  id: number;
+  author: string;
+  created_at_i: number;
+  children: NewsDetailsChildrenApiResponse[];
+  text: string;
+}
+
+export interface NewsDetailsApiResponse {
+  title: string;
+  story_id: number;
+  author: string;
+  url: string;
+  points: number;
+  created_at_i: number;
+  children: NewsDetailsChildrenApiResponse[];
+}
+
 export interface NewsItem {
   title: string;
   id: number;
@@ -17,9 +37,25 @@ export interface NewsItem {
   url: string;
   points: number;
   totalComments: number;
+  createdAt: number;
 }
 export interface NewsResult {
   page: number;
   totalPages: number;
   results: NewsItem[];
+}
+
+export interface NewsDetails {
+  title: string;
+  id: number;
+  author: string;
+  url: string;
+  points: number;
+  createdAt: number;
+  comments: NewsDetailsChildrenApiResponse[];
+}
+
+export interface PageProps {
+  params: { slug: string };
+  searchParams?: { [key: string]: string | string[] | undefined };
 }
