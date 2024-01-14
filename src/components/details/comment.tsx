4ae0adf76cecbe2805f2d0e2769efdd1e24f1e72
AbhishekUnthread <abhishek.k@unthread.world>
2024-01-14 14:51:16 +0530

    nested comments added

--- src/components/details/comment.tsx
+++ src/components/details/comment.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useEffect, useState } from 'react';
 import DOMPurify from 'dompurify';
+import { UserRound, Clock1, ChevronDown, ChevronUp } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
 import {
@@ -11,30 +12,113 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import CommentList from './comment-list';
+import { Skeleton } from '../ui/skeleton';
 
 import { NewsDetailsChildrenApiResponse } from '@/app/types';
 
+import { formatSecondsToDate } from '@/utils/time';
+import { cn } from '@/lib/utils';
+
 const Comment: React.FC<NewsDetailsChildrenApiResponse> = (props) => {
-  const { id, author, created_at_i, children, text } = props;
+  const { author, created_at_i, children, text } = props;
   const [cleanText, setCleanText] = useState('');
+  const [showReplies, setShowReplies] = useState(false);
 
   useEffect(() => {
     const sanitizedText = DOMPurify.sanitize(text);
     setCleanText(sanitizedText);
   }, [text]);
 
+  const toggleShowRepliesHandler = () => {
+    setShowReplies((prevState) => !prevState);
+  };
+
   return (
     <>
-      <Card className=''>
-        <CardHeader>
-          <CardTitle>Create project</CardTitle>
-          <CardDescription
-            dangerouslySetInnerHTML={{
-              __html: cleanText,
-            }}
-          ></CardDescription>
+      <Card>
+        <CardHeader className='grid gap-3'>
+          <div className='flex items-center justify-between'>
+            <CardTitle className='flex items-center gap-2'>
+              <UserRound size={14} />
+              {author}
+            </CardTitle>
+            <div className='flex items-center gap-2'>
+              <Clock1 size={14} />
+              <span className='font-normal text-xs'>
+                {formatSecondsToDate(created_at_i)}
+              </span>
+            </div>
+          </div>
+          {text &&
+            (!cleanText ? (
+              <Skeleton className='w-[70%] h-4' />
+            ) : (
+              <CardDescription
+                dangerouslySetInnerHTML={{
+                  __html: cleanText,
+                }}
+              ></CardDescription>
+            ))}
         </CardHeader>
+        {!!children.length && (
+          <CardFooter>
+            {showReplies ? (
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      onClick={toggleShowRepliesHandler}
+                      size='sm'
+                      className='p-1 bg-transparent hover:bg-transparent'
+                    >
+                      <ChevronUp className='text-gray-400' />
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p className='m-0'>Hide replies</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            ) : (
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      onClick={toggleShowRepliesHandler}
+                      size='sm'
+                      className='p-1 bg-transparent hover:bg-transparent'
+                    >
+                      <ChevronDown className='text-gray-400' />
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p className='m-0'>Show replies</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            )}
+          </CardFooter>
+        )}
       </Card>
+      {!!children.length && (
+        <>
+          <div className={cn('hidden gap-10', showReplies && 'flex')}>
+            <Button
+              onClick={toggleShowRepliesHandler}
+              className='w-[1px] h-full p-0 bg-slate-200'
+              aria-label='hide replies'
+            />
+            <CommentList comments={children} />
+          </div>
+        </>
+      )}
     </>
   );
 };
